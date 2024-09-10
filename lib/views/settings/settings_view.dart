f60a9c9ac165f03320a3c1dd7cba2838fa7affed
PavelDelhomme <pavel@delhomme.ovh>
2024-09-10 11:31:16 +0200

    Modification des notifications correctement fonctionnel

--- lib/views/settings/settings_view.dart
+++ lib/views/settings/settings_view.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -44,25 +45,23 @@ class SettingsViewState extends State<SettingsView> {
     });
   }
 
-  Future<void> _saveSettings() async {
+  Future<void> _updateReminderSettings(bool reminderEnabled, int reminderTime) async {
     final user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      final settingsDocRef = FirebaseFirestore.instance.collection('userSettings').doc(user.uid);
-
       try {
-        await settingsDocRef.set({
-          'reminderEnabled': _reminderEnabled,
-          'reminderTime': _reminderTime,
-        });
+        // Sauvegarde immédiate des paramètres de rappel
+        await FirebaseFirestore.instance.collection('userSettings').doc(user.uid).set({
+          'reminderEnabled': reminderEnabled,
+          'reminderTime': reminderTime,
+        }, SetOptions(merge: true));
 
-        // Mise à jour des notifications pour toutes les tâches après modification des paramètres
+        // Mise à jour des notifications après modification
         await notificationService.updateNotificationsForUser(user.uid);
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text("Paramètres enregistrés avec succès")),
-        );
+        log("Reminder settings updated : reminderEnabled : $reminderEnabled, reminderTime : $reminderTime");
       } catch (e) {
+        log("Erreur lors de la mise à jour des paramètres de rappel : $e");
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Erreur lors de l\'enregistrement des paramètres : $e')),
+          SnackBar(content: Text("Erreur lors de la mise à jour des paramètres : $e")),
         );
       }
     }
@@ -117,88 +116,87 @@ class SettingsViewState extends State<SettingsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            SwitchListTile(
-              title: const Text('Activer les rappels'),
-              value: _reminderEnabled,
-              onChanged: (value) {
-                setState(() {
-                  _reminderEnabled = value;
-                });
-              },
-            ),
-            if (_reminderEnabled)
-              ListTile(
-                title: const Text('Rappel avant la tâche (minutes)'),
-                trailing: DropdownButton<int>(
-                  value: _reminderTime,
-                  items: [5, 10, 15, 20, 30, 40, 45, 50, 55, 60]
-                      .map((int value) => DropdownMenuItem<int>(
-                    value: value,
-                    child: Text(value.toString()),
-                  ))
-                      .toList(),
-                  onChanged: (value) {
-                    if (value != null) {
-                      setState(() {
-                        _reminderTime = value;
-                      });
-                    }
-                  },
+      body: _isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              SwitchListTile(
+                title: const Text('Activer les rappels'),
+                value: _reminderEnabled,
+                onChanged: (value) {
+                  setState(() {
+                    _reminderEnabled = value;
+                  });
+                  _updateReminderSettings(value, _reminderTime);
+                },
+              ),
+              if (_reminderEnabled)
+                ListTile(
+                  title: const Text('Rappel avant la tâche (minutes)'),
+                  trailing: DropdownButton<int>(
+                    value: _reminderTime,
+                    items: [5, 10, 15, 20, 30, 40, 45, 50, 55, 60]
+                        .map((int value) => DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(value.toString()),
+                          ))
+                        .toList(),
+                    onChanged: (value) {
+                      if (value != null) {
+                        setState(() {
+                          _reminderTime = value;
+                        });
+                        _updateReminderSettings(_reminderEnabled, value);
+                      }
+                    },
+                  ),
+                ),
+              const Divider(
+                height: 40,
+                thickness: 2.5,
+                indent: 20,
+                endIndent: 20,
+                color: Colors.deepPurple,
+              ),
+              const Text(
+                "Paramètres utilisateur",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      initialValue: _userEmail,
+                      decoration: const InputDecoration(labelText: "Email"),
+                      validator: (value) => value!.isEmpty ? 'Ce champ ne peut être vide' : null,
+                      onChanged: (value) => _userEmail = value,
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(labelText: "Nouveau mot de passe"),
+                      validator: (value) => value!.isEmpty ? 'Ce champ ne peut être vide' : null,
+                      onChanged: (value) => _userPassword = value,
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _changeEmail,
+                      child: const Text('Changer l\'email'),
+                    ),
+                    ElevatedButton(
+                      onPressed: _changePassword,
+                      child: const Text('Changer le mot de passe'),
+                    ),
+                  ],
                 ),
               ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _saveSettings,
-              child: const Text('Enregistrer'),
-            ),
-            const Divider(
-              height: 40,
-              thickness: 2.5,
-              indent: 20,
-              endIndent: 20,
-              color: Colors.deepPurple,
-            ),
-            const Text(
-              "Paramètres utilisateur",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    initialValue: _userEmail,
-                    decoration: const InputDecoration(labelText: "Email"),
-                    validator: (value) => value!.isEmpty ? 'Ce champ ne peut être vide' : null,
-                    onChanged: (value) => _userEmail = value,
-                  ),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(labelText: "Nouveau mot de passe"),
-                    validator: (value) => value!.isEmpty ? 'Ce champ ne peut être vide' : null,
-                    onChanged: (value) => _userPassword = value,
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: _changeEmail,
-                    child: const Text('Changer l\'email'),
-                  ),
-                  ElevatedButton(
-                    onPressed: _changePassword,
-                    child: const Text('Changer le mot de passe'),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
     );
   }
 }
